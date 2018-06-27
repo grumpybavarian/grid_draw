9750e976bf667e803e82bb7de102f56835a7b64d
Thomas Edlich <thomas.edlich@tum.de>
2018-06-27 13:17:12 +0100

    adds sceleton of rgb env

---
+++ grid_draw/envs/grid_draw_rgb.py
@@ -0,0 +1,17 @@
+import gym
+import numpy as np
+
+class GridDrawBwEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human', close=False):
+        pass
