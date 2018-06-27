b2ed9cfca31504f60b5ab473605371ecbeb39c53
Thomas Edlich <thomas.edlich@tum.de>
2018-06-27 13:15:38 +0100

    adds init script

--- grid_draw/__init__.py
+++ grid_draw/__init__.py
@@ -1,6 +1,11 @@
 from gym.envs.registration import register
 
 register(
-    id='grid-draw-v0',
-    entry_point='grid_draw.envs:GridDrawEnv',
+    id='grid-draw-bw-v0',
+    entry_point='grid_draw.envs:GridDrawBwEnv',
+)
+
+register(
+    id='grid-draw-rgb-v0',
+    entry_point='grid_draw.envs:GridDrawRgbEnv',
 )
