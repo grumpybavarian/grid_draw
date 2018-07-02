c1758f6aa4ef191c428715ed751dbb7d6176c0be
Thomas Edlich <thomas.edlich@tum.de>
2018-07-02 12:53:25 +0100

    removes move actions, agent automatically moves forward

--- grid_draw/envs/grid_draw_bw.py
+++ grid_draw/envs/grid_draw_bw.py
@@ -8,7 +8,7 @@ class GridDrawBwEnv(gym.Env):
 
     def __init__(self):
         self.grid_size = 5
-        self.action_space = spaces.Discrete(10)
+        self.action_space = spaces.Discrete(6)
         self.observation_space = spaces.Box(low=0, high=255,
                                             shape=(2, self.grid_size, self.grid_size), dtype=np.float32)
         self.current_state = None
@@ -19,33 +19,20 @@ class GridDrawBwEnv(gym.Env):
         if self.done:
             raise RuntimeError("Episode has finished. Call env.reset() to start a new episode.")
 
-        if action == 9:
-            self.done = True
-            return self.current_state, 0, True, None
+        bw_values = [0, 50, 100, 150, 200, 250]
 
-        if action >= 4 and action <=8:
-            bw_values = [50, 100, 150, 200, 250]
-
-            self.current_state[0][tuple(self.position)] = bw_values[action - 4] / 255.
-            return self.current_state, 0, False, None
-
-        if action == 0:  # move up
-            move = np.array([-1, 0])
-        elif action == 1:  # move right
-            move = np.array([0, 1])
-        elif action == 2:  # move down
-            move = np.array([1, 0])
-        elif action == 3:  # move left
-            move = np.array([0, -1])
-        else:
-            raise ValueError("Action not contained in Action Space. The action space is: " + self.action_space)
+        self.current_state[0][tuple(self.position)] = bw_values[action] / 255.
 
         self.current_state[1][tuple(self.position)] = 0
 
-        self.position += move
-        self.position = np.clip(self.position, 0, self.grid_size-1)
+        self.position[0] += 1
+        self.position[0] %= self.grid_size
+        self.position[1] += int(self.position[0] == 0)
 
         self.current_state[1][tuple(self.position)] = 1
+
+        if self.position[0] == self.grid_size - 1 and self.position[1] == self.grid_size - 1:
+            return self.current_state, 0, True, None
 
         return self.current_state, 0, False, None
 
@@ -53,7 +40,7 @@ class GridDrawBwEnv(gym.Env):
         canvas = np.zeros((self.grid_size, self.grid_size))
         position_matrix = np.zeros((self.grid_size, self.grid_size))
 
-        self.position = np.random.randint(0, self.grid_size, (2,))
+        self.position = np.array([0, 0])
 
         position_matrix[tuple(self.position)] = 1
 
